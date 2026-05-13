f6bc1a38de36ec1432adbe5c42161424b72823e3
agrobras123-lab <agro.bras123@gmail.com>
2026-05-13 16:27:42 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,47 +1,34 @@
-const CACHE = "erp-agrobras-v4";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./favicon.png",
-  "https://unpkg.com/react@18/umd/react.production.min.js",
-  "https://unpkg.com/react-dom@18/umd/react-dom.production.min.js"
+'use strict';
+var CACHE = 'erp-agb-v4';
+var ASSETS = [
+  '/erp-agrobras/',
+  '/erp-agrobras/index.html',
+  '/erp-agrobras/manifest.json',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js'
 ];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) {
+    return Promise.all(ASSETS.map(function(u) { return c.add(u).catch(function(){}); }));
+  }));
+  self.skipWaiting();
 });
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches
-      .keys()
-      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k){return k!==CACHE}).map(function(k){return caches.delete(k)}));
+  }));
+  self.clients.claim();
 });
-
-self.addEventListener("fetch", (e) => {
-  if (e.request.method !== "GET") return;
-  if (e.request.url.includes("firestore.googleapis.com")) return;
-
-  const url = new URL(e.request.url);
-  const isAppShell = url.origin === self.location.origin && (url.pathname.endsWith("/") || url.pathname.endsWith("/index.html"));
-
-  if (isAppShell) {
-    e.respondWith(
-      fetch(e.request)
-        .then((resp) => {
-          const copy = resp.clone();
-          caches.open(CACHE).then((c) => c.put("./index.html", copy));
-          return resp;
-        })
-        .catch(() => caches.match("./index.html"))
-    );
+self.addEventListener('fetch', function(e) {
+  if(e.request.url.includes('firestore.googleapis.com')){
+    e.respondWith(fetch(e.request).catch(function(){return new Response('{}',{headers:{'Content-Type':'application/json'}})}));
     return;
   }
-
-  e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(function(cached){
+    var net=fetch(e.request).then(function(r){
+      if(r&&r.status===200&&e.request.method==='GET'){var cl=r.clone();caches.open(CACHE).then(function(c){c.put(e.request,cl)})}
+      return r;
+    }).catch(function(){return cached});
+    return cached||net;
+  }));
 });
